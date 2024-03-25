1893f6ae4c72d19f1e212fa2e451c1a4b9f6722c
NguyenQuangDat0377 <nguyenquangdat07111994@gmail.com>
2024-03-25 18:30:59 +0700

    Bài Thi GK
    Figma

---
+++ Cau2_GameTetris/app/src/main/java/ntu/a63131916/parts/Point.kt
@@ -0,0 +1,19 @@
+package ntu.a63131916;
+
+class Point(var x:Int,var y:Int){
+
+    fun moveDown(){
+        x++
+    }
+    fun moveLeft(){
+        y--
+    }
+
+    fun moveRight(){
+        y++
+    }
+
+    fun moveTop(){
+        x--
+    }
+}
