6350140fd5888c611106a56e2b5a36279a9c7aa4
NguyenQuangDat0377 <nguyenquangdat07111994@gmail.com>
2024-03-25 18:05:48 +0700

    Bài Thi GK
    Giao diện

---
+++ Cau2_GameTetris/app/src/main/java/ntu/a63131916/basic_information_activity.java
@@ -0,0 +1,80 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		basic_information
+	 *	@date 		Sunday 24th of March 2024 03:37:43 AM
+	 *	@title 		Cover
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class basic_information_activity extends Activity {
+
+	
+	private View _bg__basic_information_ek2;
+	private View _bg__group_41_ek1;
+	private View ellipse_36;
+	private View ellipse_40;
+	private View ellipse_41;
+	private View ellipse_39;
+	private View ellipse_37;
+	private View ellipse_38;
+	private View _bg__group_21_ek1;
+	private View _bg__group_20_ek1;
+	private TextView thank_you_for_showing_interest_in_the_file;
+	private TextView to_play_tetris_with_this_file__you_must_have_the_figma_tetris_plugin_installed__https___www_figma_com_community_plugin_831000487189051522_figma_tetris___go_onto_the__play__page__and_run_the_plugin_and_have_fun____;
+	private ImageView image_2;
+	private View _bg__frame_2_ek1;
+	private View _bg__frame_1_ek1;
+	private TextView plugin_and_component_credits_;
+	private TextView emily_zhong;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.basic_information);
+
+		
+		_bg__basic_information_ek2 = (View) findViewById(R.id._bg__basic_information_ek2);
+		_bg__group_41_ek1 = (View) findViewById(R.id._bg__group_41_ek1);
+		ellipse_36 = (View) findViewById(R.id.ellipse_36);
+		ellipse_40 = (View) findViewById(R.id.ellipse_40);
+		ellipse_41 = (View) findViewById(R.id.ellipse_41);
+		ellipse_39 = (View) findViewById(R.id.ellipse_39);
+		ellipse_37 = (View) findViewById(R.id.ellipse_37);
+		ellipse_38 = (View) findViewById(R.id.ellipse_38);
+		_bg__group_21_ek1 = (View) findViewById(R.id._bg__group_21_ek1);
+		_bg__group_20_ek1 = (View) findViewById(R.id._bg__group_20_ek1);
+		thank_you_for_showing_interest_in_the_file = (TextView) findViewById(R.id.thank_you_for_showing_interest_in_the_file);
+		to_play_tetris_with_this_file__you_must_have_the_figma_tetris_plugin_installed__https___www_figma_com_community_plugin_831000487189051522_figma_tetris___go_onto_the__play__page__and_run_the_plugin_and_have_fun____ = (TextView) findViewById(R.id.to_play_tetris_with_this_file__you_must_have_the_figma_tetris_plugin_installed__https___www_figma_com_community_plugin_831000487189051522_figma_tetris___go_onto_the__play__page__and_run_the_plugin_and_have_fun____);
+		image_2 = (ImageView) findViewById(R.id.image_2);
+		_bg__frame_2_ek1 = (View) findViewById(R.id._bg__frame_2_ek1);
+		_bg__frame_1_ek1 = (View) findViewById(R.id._bg__frame_1_ek1);
+		plugin_and_component_credits_ = (TextView) findViewById(R.id.plugin_and_component_credits_);
+		emily_zhong = (TextView) findViewById(R.id.emily_zhong);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
